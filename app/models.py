e297a737bc370a4f95a6e756162365278452b5d8
Muhammad <bashirmusa748@gmail.com>
2022-05-15 12:46:48 +0100

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from email.policy import default
 from datetime import datetime
 import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+#from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from markdown import markdown
 import bleach
 from flask import current_app, request, url_for
